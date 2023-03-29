43741c62d3c56df2ef33acf221163ce3314ed158
Emilio <emilio.pg95@gmail.com>
2023-03-29 11:34:28 -0600

    🔧 Improve functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ class App {
         this.$noteTitle = document.querySelector("#note-title");
         this.$noteText = document.querySelector("#note-text");
         this.$formButtons = document.querySelector("#form-buttons");
+        this.$formCloseButton = document.querySelector('#form-close-button')
 
         this.addEventListeners();
     }
@@ -15,9 +16,9 @@ class App {
     addEventListeners() {
         document.body.addEventListener("click", event => {
         this.handleFormClick(event);
-      });
-      
-      this.$form.addEventListener('submit', event => {
+    });
+    
+    this.$form.addEventListener('submit', event => {
         event.preventDefault(); 
         const title = this.$noteTitle.value;   
         const text = this.$noteText.value;   
@@ -25,25 +26,36 @@ class App {
         if (hasNote) {
             this.addNote({ title, text })
         }
-      });
+    });
+
+    this.$formCloseButton.addEventListener('click', event => {
+        event.stopPropagation();
+        this.closeForm();
+    })
     }
-  
+
     handleFormClick(event) {
         const isFormClicked = this.$form.contains(event.target);
-  
+
+        const title = this.$noteTitle.value;
+        const text = this.$noteText.value;
+        const hasNote = title || text
+
         if (isFormClicked) {
             this.openForm();
+        } else if (hasNote) {
+            this.addNote({ title, text });
         } else {
             this.closeForm();
         }
     }
-  
+
     openForm() {
         this.$form.classList.add("form-open");
         this.$noteTitle.style.display = "block";
         this.$formButtons.style.display = "block";
     }
-  
+
     closeForm() {
         this.$form.classList.remove("form-open");
         this.$noteTitle.style.display = "none";
@@ -52,10 +64,10 @@ class App {
         this.$noteText.value = '';
     }
     
-    addNote(note) {
+    addNote({ title, text }) {
         const newNote = {
-            title: note.title,
-            text: note.text,
+            title,
+            text,
             color: 'white',
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
         };
@@ -81,7 +93,6 @@ class App {
             </div>
         `).join("");
     }
-  }
-  
-  new App();
-  
+}
+
+new App();
